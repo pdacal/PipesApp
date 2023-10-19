ea5c33af5dc99f4c9c5ef1c8c23956cd552438bd
pdacal <pauladacalm@gmail.com>
2023-10-19 10:13:47 +0200

    pipesApp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// carga perezosa, cargamos asi o producst. module, que dentro del vai importado o products-routing.module onde estan as rutas declaradas das paxinas
+const routes: Routes = [
+{
+  path: '',
+  loadChildren: () => import('./products/products.module').then( m => m.ProductsModule),
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
+import localeEs from "@angular/common/locales/es"; //epanhol
+import localeFr from "@angular/common/locales/fr"; //frances
+import localeAr from "@angular/common/locales/ar"; //arabe
+import localeJa from "@angular/common/locales/ja"; //arabe
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData( localeEs );
+registerLocaleData( localeFr );
+registerLocaleData( localeAr );
+registerLocaleData( localeJa );
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    //para establecer idioma por defecto en TODA a app
+    {provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
